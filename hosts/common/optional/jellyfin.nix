f168edbdb07bae0f1e945272cb342302fe6a9a53
c3r5b8 <olespid@gmail.com>
2023-12-21 22:26:39 +0200

    [fix]: jellyfin without container

--- hosts/common/optional/jellyfin.nix
+++ hosts/common/optional/jellyfin.nix
@@ -1,18 +1,32 @@
-{config, ...}: {
-  virtualisation.oci-containers = {
-    containers = {
-      jellyfin = {
-        image = "lscr.io/linuxserver/jellyfin";
-        autoStart = true;
-        volumes = [
-          "/mnt/fat_ssd/Media/TV:/data/tvshows"
-          "/mnt/fat_ssd/Media/Movies:/data/movies"
-          "/mnt/fat_ssd/jellyfin:/config"
-        ];
-        environment = {
-          TZ = "Europe/Kyiv";
-        };
-      };
-    };
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # virtualisation.oci-containers = {
+  #   containers = {
+  #     jellyfin = {
+  #       image = "lscr.io/linuxserver/jellyfin";
+  #       autoStart = true;
+  #       volumes = [
+  #         "/mnt/fat_ssd/Media/TV:/data/tvshows"
+  #         "/mnt/fat_ssd/Media/Movies:/data/movies"
+  #         "/mnt/fat_ssd/jellyfin:/config"
+  #       ];
+  #       environment = {
+  #         TZ = "Europe/Kyiv";
+  #       };
+  #     };
+  #   };
+  # };
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
   };
 }

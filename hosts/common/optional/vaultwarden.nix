be6de47ef679470c5dec5a1dcceca546e63ed993
c3r5b8 <olespid@gmail.com>
2023-12-25 20:02:33 +0200

    [feat]: add vaultwarden

---
+++ hosts/common/optional/vaultwarden.nix
@@ -0,0 +1,22 @@
+{config, ...}: {
+  virtualisation.oci-containers = {
+    containers = {
+      vaultwarden = {
+        image = "vaultwarden/server:latest";
+        autoStart = true;
+        extraOptions = [
+          "-l=traefik.enable=true"
+          "-l=traefik.http.routers.vaultwarden.rule=Host(`pass.pass.c3r5b8.dev`)"
+          "-l=traefik.http.services.vaultwarden.loadbalancer.server.port=80"
+        ];
+        volumes = [
+          "/mnt/fat_ssd/vaultwarden:/data"
+        ];
+        environment = {
+          DOMAIN = "https://pass.c3r5b8.dev";
+          WEBSOCKET_ENABLED = "true";
+        };
+      };
+    };
+  };
+}

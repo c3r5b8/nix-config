3169b46118d32ecf7f9743331b042e38beec8f9a
c3r5b8 <olespid@gmail.com>
2023-12-29 12:58:31 +0200

    [fix]: change secrets

--- hosts/common/global/sops.nix
+++ hosts/common/global/sops.nix
@@ -4,4 +4,5 @@
   sops.defaultSopsFile = ../secrets.yaml;
   sops.age.sshKeyPaths = ["/home/c3r5b8/.ssh/id_ed25519"];
   sops.secrets.cloudflareDnsApiCredentials = {};
+  sops.secrets.postgres = {};
 }

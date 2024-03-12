989abec8ef357667a1259d3ef5371736831c19ab
c3r5b8 <olespid@gmail.com>
2024-03-12 14:18:17 +0200

    [feat]: change docker to podman

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,4 +1,4 @@
-{
+{pgs,...}:{
   virtualisation = {
     podman = {
       enable = true;
@@ -6,6 +6,26 @@
     };
     oci-containers = {
       backend = "podman";
+    };
+  };
+  systemd = {
+    services = {
+      podman-out = {
+        enable = true;
+        description = "Send podman socet in network";
+        script = "${pkgs.podman}/bin/podman system service --time=0 tcp://0.0.0.0:37017";
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = "1";
+        };
+        wantedBy = ["multi-user.target"];
+      };
+    };
+  };
+  networking = {
+    firewall = {
+      allowedTCPPorts = [ 37017 ];
     };
   };
 }

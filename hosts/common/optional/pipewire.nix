e8a2346da8c58a9afadb1bbe31b62952a68239c7
c3r5b8 <olespid@gmail.com>
2024-03-26 19:21:21 +0200

    [chore]: update config

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -1,16 +1,18 @@
-{
+{pkgs, ...}: {
   services.pipewire = {
     enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/main.lua.d/51-bluez-config.lua" ''
+          bluez_monitor.properties = {
+          	["bluez5.enable-hw-volume"] = false,
+          }
+        '')
+      ];
+    };
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-  # environment.etc = {
-  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-  #     bluez_monitor.properties = {
-  #     	["bluez5.enable-hw-volume"] = false,
-  #     }
-  #   '';
-  # };
 }

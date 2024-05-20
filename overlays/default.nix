4c17e2defadda17b29d92bd18d806eb0ba6ecdca
c3r5b8 <olespid@gmail.com>
2024-05-20 18:16:57 +0300

    [feat]: add pihole

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,16 +1,17 @@
-{inputs}: {
+{ inputs, outputs, ... }: {
   flake-inputs = final: _: {
     inputs =
       builtins.mapAttrs
-      (
-        _: flake: let
-          legacyPackages = (flake.legacyPackages or {}).${final.system} or {};
-          packages = (flake.packages or {}).${final.system} or {};
-        in
-          if legacyPackages != {}
-          then legacyPackages
-          else packages
-      )
-      inputs;
+        (
+          _: flake:
+            let
+              legacyPackages = (flake.legacyPackages or { }).${final.system} or { };
+              packages = (flake.packages or { }).${final.system} or { };
+            in
+            if legacyPackages != { }
+            then legacyPackages
+            else packages
+        )
+        inputs;
   };
 }

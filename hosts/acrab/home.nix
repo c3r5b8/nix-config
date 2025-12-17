b3596d07cfd289e672668e20ea7352ca30a48d12
c3r5b8 <olespid@gmail.com>
2025-12-17 10:31:50 +0200

    [feat]: add keys for other hosts, add monitor settings for acrab

--- hosts/acrab/home.nix
+++ hosts/acrab/home.nix
@@ -2,29 +2,8 @@
   imports = [
     ../../home/c3r5b8
   ];
-#   wayland.windowManager.sway.config.output = {
-#     "eDP-1" = {
-#       resolution = "3072x1920@120Hz";
-#       bg = "${theme.wallpapers} fill";
-#       pos = "0 0";
-#       scale = "1.6";
-#       scale_filter = "linear";
-#       subpixel = "rgb";
-#     };
-#     "HEADLESS-1" = {
-#       resolution = "2560x1600@120Hz";
-#       pos = "1920 1080";
-#       scale = "2";
-#       subpixel = "rgb";
-#     };
-#     "HDMI-A-1" = {
-#       pos = "1920 0";
-#       subpixel = "rgb";
-#     };
-#   };
-  # nix.settings.experimental-features = ["nix-command" "flakes"];
-  # home.username = "c3r5b8";
-  # home.homeDirectory = "/home/c3r5b8";
-  # home.stateVersion = "25.05";
-  # programs.home-manager.enable = true;
+  xdg.configFile."monitors.xml" = {
+    source = ./monitors.xml;
+  };
+
 }

bb7ac8153e8e102ebe50fc74db5d11233c493d59
c3r5b8 <olespid@gmail.com>
2026-03-31 21:13:34 +0300

    [feat]: update fish config

--- home/c3r5b8/fish/default.nix
+++ home/c3r5b8/fish/default.nix
@@ -3,8 +3,29 @@
     fish = {
       enable = true;
       interactiveShellInit = ''
-        set fish_greeting # Disable greeting
+        set fish_greeting
+        function ssh
+          set -lx TERM xterm-256color
+          command ssh $argv
+        end
+
       '';
+      shellAbbrs = {
+        gcm = "git commit -m";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        "...." = "cd ../../..";
+        "....." = "cd ../../../..";
+        "......" = "cd ../../../../..";
+      };
+      shellAliases = {
+        ll = "ls -lah";
+        gs = "git status";
+        gco = "git checkout";
+        vim = "nvim";
+        top = "btop";
+        cat = "bat";
+      };
     };
   };
 }

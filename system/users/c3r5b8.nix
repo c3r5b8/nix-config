6232760da33745afbcbed91dc977a6a7b8fe9a4d
c3r5b8 <olespid@gmail.com>
2026-04-18 14:35:29 +0300

    [feat]: ssh keys

--- system/users/c3r5b8.nix
+++ system/users/c3r5b8.nix
@@ -22,5 +22,11 @@ in {
       "video"
       "wheel"
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDrGeP6Qe0seU9JJe1wiTY7/mpfRcoZPfHt70MZQ/Raq c3r5b8@antares"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL1GRusiqCwdhdksxl1Of0xi+cYOVMo7ipvx58ZT44k7 c3r5b8@acrab"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOE6+pFtIyCjiuhKw7UOtBsMV1VmXCLMJm1wpg7jZay c3r5b8@shaula"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErdNJIs3/2rgAuKgihmocXqWGtYkjd24YrVjREuvTca c3r5b8@shaula"
+    ];
   };
 }

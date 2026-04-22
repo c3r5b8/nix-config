9e4224c08be5329b931dda4253d258b2059e126c
c3r5b8 <olespid@gmail.com>
2026-04-22 10:36:56 +0300

    feat: make auto updates wait for ac power

--- system/updates/default.nix
+++ system/updates/default.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   system.autoUpgrade = {
     enable = true;
     flake = "github:c3r5b8/nix-config#${config.networking.hostName}";
@@ -11,6 +15,27 @@
   };
 
   systemd.services.nixos-upgrade = {
-    unitConfig.ConditionACPower = true;
+    preStart = let
+      waitForAC = pkgs.writeShellScript "wait-for-ac-power" ''
+        #!/usr/bin/env bash
+        set -euo pipefail
+
+        echo "nixos-upgrade: Checking AC power..."
+
+        timeout=43200
+
+        while ! ${pkgs.systemd}/bin/systemd-ac-power; do
+          echo "nixos-upgrade: On battery — waiting 120s for AC power..."
+          sleep 120
+          timeout=$((timeout - 120))
+          if [ "$timeout" -le 0 ]; then
+            echo "nixos-upgrade: Timeout waiting for AC power — skipping upgrade today"
+            exit 1
+          fi
+        done
+
+        echo "nixos-upgrade: AC power detected — proceeding with upgrade!"
+      '';
+    in "${waitForAC}";
   };
 }

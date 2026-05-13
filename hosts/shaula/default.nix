8920f78dbab3387b85013cdc63b17c84ce11ab6e
c3r5b8 <olespid@gmail.com>
2026-05-13 21:17:31 +0300

    shaula: make copilot key to work as ctrl

--- hosts/shaula/default.nix
+++ hosts/shaula/default.nix
@@ -55,6 +55,21 @@
       gtklock-virtkb-module
     ];
   };
+
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = ["0001:0001:09b4e68d"];
+        settings = {
+          main = {
+            "leftshift+leftmeta+f23" = "layer(control)";
+          };
+        };
+      };
+    };
+  };
+
   services.logind.settings.Login.HandlePowerKey = "suspend";
   services.linux-enable-ir-emitter.enable = true;
   programs.sway.enable = true;

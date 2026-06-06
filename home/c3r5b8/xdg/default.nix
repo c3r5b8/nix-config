3080b647a213d0f484644b9daa75bc437ccab92f
c3r5b8 <olespid@gmail.com>
2026-06-06 17:24:06 +0300

    xdg: add bookmarks

--- home/c3r5b8/xdg/default.nix
+++ home/c3r5b8/xdg/default.nix
@@ -11,4 +11,10 @@
     pictures = "$HOME/pictures";
     videos = "$HOME/videos";
   };
+  xdg.configFile."gtk-3.0/bookmarks".text = ''
+    sftp://c3r5b8@sargas/ sargas
+    sftp://c3r5b8@antares/ antares
+    sftp://c3r5b8@shaula/ shaula
+    sftp://c3r5b8@acrab/ acrab
+  '';
 }

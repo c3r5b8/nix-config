025d1578ab13de7bd7b91f6b030a27dc6e7b8e0a
c3r5b8 <olespid@gmail.com>
2026-06-27 19:01:16 +0300

    hosts: block battleye for gta online

--- system/steam/default.nix
+++ system/steam/default.nix
@@ -5,4 +5,11 @@
       proton-ge-bin
     ];
   };
+  networking.hosts = {
+    "0.0.0.0" = [
+      "paradise-s1.battleye.com"
+      "test-s1.battleye.com"
+      "paradiseenhanced-s1.battleye.com"
+    ];
+  };
 }

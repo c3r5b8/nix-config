656e775467dbe0a6a5d63043db5ec7a3ff9d12d3
c3r5b8 <olespid@gmail.com>
2026-06-28 22:48:44 +0300

    btop: show per cpu load

--- home/c3r5b8/btop/default.nix
+++ home/c3r5b8/btop/default.nix
@@ -5,6 +5,7 @@
       color_theme = "theme";
       update_ms = 1000;
       proc_sorting = "cpu direct";
+      proc_per_core = true;
     };
   };
   xdg.configFile."btop/themes/light.theme".source = ./light.theme;
